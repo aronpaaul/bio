17fb0af04753a03079bb016bbf439dfab5b9a3cd
aronpaaul <261238353+aronpaaul@users.noreply.github.com>
2026-07-25 19:52:48 +0300

    block cyrillic meowclient ad variants

--- server/security/spamFilter.ts
+++ server/security/spamFilter.ts
@@ -1,5 +1,5 @@
 const linkRe = /(https?:\/\/|www\.|t\.me|telegram\.me|@[a-z0-9_]{3,}|\b[a-z0-9-]+\.(com|ru|net|org|io|gg|me|xyz|tg|top|club|link|shop)\b)/i
-const wordRe = /(meow\s*client|meowclient|discord\.gg|join\s+my|подпишись|subscribe|промокод|promo\s*code)/i
+const wordRe = /(meow\s*client|meowclient|мяу\s*кл[иi]ент|мяукл[иi]ент|кошка\s*кл[иi]ент|кошкакл[иi]ент|киска\s*кл[иi]ент|discord\.?gg|join\s+my|подпишись|subscribe|промокод|promo\s*code)/i
 
 export function spamReason(name: string, message: string): string | null {
   const text = (name + ' ' + message).normalize('NFKC').toLowerCase()
